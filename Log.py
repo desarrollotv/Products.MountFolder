a8ec4f92cac9eb2f355bc41d18bc2cef8c5c0e0c
kayeva <kayeva@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2006-01-25 22:52:57 +0000

    Updated header of all .py files. Deleted unnecessary __init__.py file.
    git-svn-id: http://svn.plone.org/svn/collective/MountFolder/trunk@18490 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Log.py
+++ Log.py
@@ -1,7 +1,23 @@
-# ****************************
-# ****** LOG FACILITIES ******
-# ****************************
+# -*- coding: utf-8 -*-
+## MountFolder : AT-based Folder that can be used as the root container for a mounted database.
+## Copyright (C)2005 Ingeniweb
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """
+Log facilities
+
 One can override the following variables :
 
 LOG_LEVEL : The log level, from 0 to 5.
@@ -20,7 +36,9 @@ LOG_DEBUG = 5           => Debug (Debugging information)
 LOG_PROCESSOR : A dictionnary holding, for each key, the data processor.
 A data processor is a function that takes only one parameter : the data to print.
 Default : LogFile for all keys.
+
 """
+__docformat__ = 'restructuredtext'
 
 
 LOG_LEVEL = -1
--- tests/testMountFolder.py
+++ tests/testMountFolder.py
@@ -1,18 +1,25 @@
-##############################################################################
-#
-# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
-#
-##############################################################################
-"""Tests of MountFolder
+# -*- coding: utf-8 -*-
+## MountFolder : AT-based Folder that can be used as the root container for a mounted database.
+## Copyright (C)2005 Ingeniweb
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """
+Tests of MountFolder
+"""
+__docformat__ = 'restructuredtext'
+
 
 import os
 import sys
