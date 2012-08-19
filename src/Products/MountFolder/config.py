512685e0634f9a708af7bb815f7809c39098a4cd
Juan A. Diaz <juan@linux.org.ar>
2012-08-19 02:42:36 -0300

    Cleanup.

--- src/Products/MountFolder/config.py
+++ src/Products/MountFolder/config.py
@@ -18,31 +18,7 @@
 """
 MountFolder config module
 """
-__docformat__ = 'restructuredtext'
 
-
-import os
-
-
-PROJECTNAME = "MountFolder"
+PROJECTNAME = 'Products.MountFolder'
 GLOBALS = globals()
 SKINS_DIR = 'skins'
-
-# Check if we have to be in debug mode
-import Log
-if os.path.isfile(os.path.abspath(os.path.dirname(__file__)) + '/debug.txt'):
-    Log.LOG_LEVEL = Log.LOG_DEBUG
-else:
-    Log.LOG_LEVEL = Log.LOG_NOTICE
-
-from Log import *
-Log = Log
-Log(LOG_NOTICE, "Starting %s at %d debug level" % (os.path.dirname(__file__), LOG_LEVEL, ))
-
-
-# Retrieve version
-if os.path.isfile(os.path.abspath(os.path.dirname(__file__)) + '/version.txt'):
-    __version_file_ = open(os.path.abspath(os.path.dirname(__file__)) + '/version.txt', 'r', )
-    version__ = __version_file_.read()[:-1]
-else:
-    version__ = "(UNKNOWN)"
